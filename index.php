abe7ccb7a8a67d9bd2c696e340f69e11b57a40b2
jespersliedrecht <74653040+jespersliedrecht@users.noreply.github.com>
2020-12-11 22:22:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo "Voornaam". "<br>";
+echo "Achternaam". "<br>";
+echo "Telefoonnummer". "<br>";
+echo "Email". "<br>";
+echo "Woonplaats". "<br>";
+echo "Adres". "<br>";
+echo "Postcode". "<br>";
+echo "Type Afspraak". "<br>";
+echo "Weeknummer". "<br>";
+echo "Datum". "<br>";
+echo "Tijd". "<br>";
+echo "Opmerkingen". "<br>";
+?>
