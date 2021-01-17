4a340e6f8f22c21d77a14bd800ded84fd8cd690f
jespersliedrecht <74653040+jespersliedrecht@users.noreply.github.com>
2021-01-17 01:15:55 +0100

    Add files via upload
    De nieuwste files (17-1-2021)

--- index.php
+++ index.php
@@ -1,14 +1,86 @@
 <?php
-echo "Voornaam". "<br>";
-echo "Achternaam". "<br>";
-echo "Telefoonnummer". "<br>";
-echo "Email". "<br>";
-echo "Woonplaats". "<br>";
-echo "Adres". "<br>";
-echo "Postcode". "<br>";
-echo "Type Afspraak". "<br>";
-echo "Weeknummer". "<br>";
-echo "Datum". "<br>";
-echo "Tijd". "<br>";
-echo "Opmerkingen". "<br>";
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+
+    <title>Maak een Afspraak</title>
+    <h1>Maak een Afspraak</h1>
+
+    <script src="script1.js"></script>
+    <link rel="stylesheet" href="style.css" />
+
+
+    <title>Kies een datum</title>
+
+
+    <link
+            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+
+</head>
+
+<body1>
+
+
+<div id="center">
+    <form>
+        <label for="dat">Datum:</label>
+        <br>
+        <input type="date" id="dat" name="dat">
+        <br>
+        <label for="tijd">Tijd</label>
+        <br>
+        <input type="time" id="tijd" name="tijd"
+        <br>
+        <br>
+        <label for="vn">Voornaam:</label>
+        <br>
+        <input type="text" id="vn" name="vn">
+        <br>
+        <label for="an">Achternaam:</label>
+        <br>
+        <input type="text" id="an" name="an">
+        <br>
+        <label for="wpl">Woonplaats:</label>
+        <br>
+        <input type="text" id="wpl" name="wpl">
+        <br>
+        <label for="adres">Adres:</label>
+        <br>
+        <input type="text" id="adres" name="adres">
+        <br>
+        <label for="email">Email-Adres:</label>
+        <br>
+        <input type="email" id="email" name="email">
+        <br>
+        <label for="tele">Telefoonnummer:</label>
+        <br>
+        <input type="tel" id="tele" name="tele" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+        <br>
+        <label for="opm">Opmerkingen:</label>
+        <br>
+        <input type="text" id="opm" name="opm">
+        <br>
+        <input id="Verzenden" type="button" name="Verzenden" value="Verzenden">
+    </form>
+</div>
+</body1>
+
+
+<body2>
+
+
+
+
+</body2>
+
+
+
+
+
+
+</html>
